9263795349baf7e3d0d64fe61966c0791b4c6bdd
Vitor Boccio - VBO <vitor.boccio@iteris.local>
2016-09-28 15:31:47 -0300

    chamada de api para pagina de perfil

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -29,13 +29,6 @@ angular.module('starter.controllers', [])
 .controller('PlaylistsCtrl', function($scope, $http, $rootScope) {
     $scope.cards = [];
 
-    $rootScope.imgCardBg;
-    $rootScope.authorCard;
-    $rootScope.essayCard;
-    $rootScope.countUpCard;
-    $rootScope.commentsNumber;
-    $rootScope.countDownCard;
-
     $http({
         method: 'GET',
         url: "http://www.mocky.io/v2/57e976ff0f00007d1f843a91"
@@ -43,7 +36,7 @@ angular.module('starter.controllers', [])
         for (var i in response.data) {
             $scope.cards.push(response.data[i]);
         }
-       
+        console.log($scope.cards)
     })
 
 })
@@ -58,10 +51,23 @@ angular.module('starter.controllers', [])
         for (var i in response.data) {
             $scope.cardDetail.push(response.data[i]);
         }
-        
+        console.log($scope.cardDetail);
     })
 })
 
 .controller('profileCtrl', function($scope, $http, $rootScope) {
-    $scope.bigProfilePic = "http://tilomitra.com/wp-content/uploads/2014/08/avatar-cartoon.png"
+    $scope.profileDetail = [];
+    $scope.profileDetailPrevOmens = []
+
+    $http({
+        method: "GET",
+        url: "http://www.mocky.io/v2/57ebde9c110000251b2d3672"
+    }).then(function(response){
+        $scope.profileDetail = response.data;
+        console.log($scope.profileDetail);
+
+        for (var i in response.data.previousOmen) {
+            $scope.profileDetailPrevOmens.push(response.data.previousOmen[i])
+        }
+    })
 })
